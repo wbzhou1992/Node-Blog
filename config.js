697ef8ea74d4c1947e66488c414aa75d4213e550
wbzhou1988 <wbzhou1988@163.com>
2017-07-12 22:23:16 +0800

    delete node_modules

--- config.js
+++ config.js
@@ -1,26 +1,12 @@
-// config files:
-const defaultConfig = './config-default.js';
-const overrideConfig = './config-override.js';
-const testConfig = './config-test.js';
 
-const fs = require('fs');
-
-var config = null;
-
-if (process.env.NODE_ENV === 'test') {
-    console.log(`Load ${testConfig}...`);
-    config = require(testConfig);
-} else {
-    console.log(`Load ${defaultConfig}...`);
-    config = require(defaultConfig);
-    try {
-        if (fs.statSync(overrideConfig).isFile()) {
-            console.log(`Load ${overrideConfig}...`);
-            config = Object.assign(config, require(overrideConfig));
-        }
-    } catch (err) {
-        console.log(`Cannot load ${overrideConfig}.`);
-    }
-}
+var config = {
+    dialect: 'mysql',
+    database: 'nodejs',
+    username: 'wbzhou1992',
+    password: 'www',
+    host: 'localhost',
+    port: 3306
+};
 
 module.exports = config;
+
